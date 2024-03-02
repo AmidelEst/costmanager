a3911d1c7b1000d849a0388b43765d3b300a8edd
apop <amitpom14@gmail.com>
2024-03-02 16:11:04 +0200

    Creating mongoDB schemas

---
+++ src/models/Costs.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const costSchema = new mongoose.Schema(
+	{
+		user_id: {
+			type: mongoose.Schema.Types.ObjectId,
+			// costs are related to users through user_id, indicating a one-to-many relationship (one user, many costs).
+			ref: 'User',
+			required: true,
+		},
+		year: { type: Number, required: true },
+		month: { type: Number, required: true, min: 1, max: 12 },
+		day: { type: Number, required: true, min: 1, max: 31 },
+		id: { type: Number, required: true, unique: true }, // Auto-generated, unique
+		description: { type: String, required: true },
+		category: {
+			type: String,
+			required: true,
+			enum: [
+				'food',
+				'health',
+				'housing',
+				'sport',
+				'education',
+				'transportation',
+				'other',
+			],
+		},
+		sum: { type: Number, required: true },
+	},
+	{ timestamps: true }
+);
+
+const Cost = mongoose.model('Cost', costSchema);
---
+++ src/models/Users.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema(
+	{
+		// Auto-generated, unique
+		id: { type: Number, required: true, unique: true },
+		first_name: { type: String, required: true },
+		last_name: { type: String, required: true },
+		birthday: { type: Date, required: true },
+	},
+	{ timestamps: true }
+);
+
+const User = mongoose.model('User', userSchema);
